450dba50fcd3f848e27c8e061c401dfe6941b5f6
Phoebe <phoebehoward@protonmail.com>
2020-12-07 20:32:53 +0000

    Fix models and migrate

--- ranking/models.py
+++ ranking/models.py
@@ -3,14 +3,14 @@ from django.conf import settings
 from django.utils import timezone
 
 class Film(models.Model):
-    IMDb_id = models.CharField()
+    IMDb_id = models.CharField(max_length=9)
 
 class Review(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     film = models.ForeignKey(Film, on_delete=models.CASCADE)
     text = models.TextField()
     date = models.DateTimeField(default=timezone.now)
-    previous = models.OneToOneField('self', null=True, blank=True, related_name="next")
+    previous = models.OneToOneField('self', null=True, blank=True, related_name="next", on_delete=models.SET_NULL)
 
     def publish(self):
         self.date = timezone.now()
