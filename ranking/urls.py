a3aadc85eaa10a7afa6c72f67927e8ca174bf56e
Jonathan Haigh <jonathanhaigh0@gmail.ocm>
2020-12-07 21:58:09 +0000

    urls and views

--- ranking/urls.py
+++ ranking/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('', views.review_list, name='review_list'),
+    path('film/<int:pk>/', views.film, name='film'),
 ]
--- ranking/views.py
+++ ranking/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Film, Review
 
-# Create your views here.
+def film(request, pk):
+    film = get_object_or_404(Film, pk=pk)
+    return render(request, 'ranking/film.html', {'film': film})
+    
+def review_list(request):
+    reviews = Review.objects.order_by('date')
+    return render(request, 'ranking/review_list.html', {'reviews': reviews})
