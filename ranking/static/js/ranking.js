c0bb6ce8185028966bc3a76fe9337654dad61898
Phoebe <phoebehoward@protonmail.com>
2020-12-09 18:44:54 +0000

    Local storage of api response

--- ranking/static/js/ranking.js
+++ ranking/static/js/ranking.js
@@ -1,16 +1,26 @@
 $('div.film').each( function() {
 	var film = $(this);
-	var id = film.attr('id');
-		
-    var settings = {
-        "url": "https://imdb-api.com/en/API/Title/k_4075zegb/" + id,
-        "method": "GET",
-        "timeout": 0,
-    };
-	
-    $.ajax(settings).done(function (response) {
-        film.find('.title').text(response.title);
-        film.find('.poster').attr('src', response.image);
-    });
+    var id = film.attr('id');
+    
+    var api_response = localStorage.getItem(id);
+
+    if (!api_response) {
+
+        var settings = {
+            "url": "https://imdb-api.com/en/API/Title/k_4075zegb/" + id,
+            "method": "GET",
+            "timeout": 0,
+        };
+
+        $.ajax(settings).done(function (response) {
+            api_response = response;
+            localStorage.setItem(id, JSON.stringify(response));
+        });
+    } else {
+        api_response = JSON.parse(api_response);
+    }
+        
+    film.find('.title').text(api_response.title);
+    film.find('.poster').attr('src', api_response.image);
 
 });
